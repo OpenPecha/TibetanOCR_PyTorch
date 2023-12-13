8ef6bbadaebb10e2665977f98e9c6e2229d331b4
Eric86y <eric.werner.mail@gmail.com>
2023-12-13 17:53:26 +0100

    Revert "fixes to Wylie encoding"
    This reverts commit 9eef6edf327cd820f80fc44dbff1871912978f80.

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-DEFAULT_CHARSET = " !#%'()+-./0123456789:=?@ADHIMNRSTUWXY[\\]_abcdefghijklmnoprstuwyz|~§&äüö<>;"
-OLD_DEFAULT_CHARSET = " !#%'()+-./0123456789:=?@ADHIMNRSTUWXY[\\]_abcdefghijklmnoprstuwyz|~&äüö"
+DEFAULT_CHARSET = "!#%'()+-./0123456789:=?@ADHIMNRSTUWXY[\\]_abcdefghijklmnoprstuwyz|~§&äüö;"
+OLD_DEFAULT_CHARSET = " !#%'()+-./0123456789:=?@ADHIMNRSTUWXY[\\]_abcdefghijklmnoprstuwyz|~&äüö<>;"
